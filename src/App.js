3de2131be77f491ae8760458307278ccd6cdf6e1
shiv <00shiv0000@gmail.com>
2021-02-07 19:53:38 +0530

    project setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './screens/home/home';
+import theme from './theme/theme';
+import { ThemeProvider, Typography } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return( 
+    <ThemeProvider theme={theme}>
+
+        <Typography color="primary">
+        Hi there Guys !!
+        </Typography>
+        </ThemeProvider>
+    );
 }
 
 export default App;
